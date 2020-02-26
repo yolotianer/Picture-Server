b7fbb7da5d6a631189244d194c6dbec03d611086
yologithup <18821769219@163.com>
2020-02-26 19:38:13 +0800

    -

--- imageServer/src/test/java/TestGson.java
+++ imageServer/src/test/java/TestGson.java
@@ -7,6 +7,13 @@ import java.util.HashMap;
  * @author yolo
  * @date 2020/2/26-15:51
  */
+class Hero {
+    public String name;
+    public String skill1;
+    public String skill2;
+    public String skill3;
+    public String skill4;
+}
 public class TestGson {
     public static void main(String[] args) {
         HashMap<String,Object>hashMap=new HashMap<>();
@@ -16,13 +23,19 @@ public class TestGson {
         hashMap.put("skill3","加攻击并吸血");
         hashMap.put("skill4","加攻速");
 
+        Hero hero = new Hero();
+        hero.name = "曹操";
+        hero.skill1 = "剑气";
+        hero.skill2 = "三段跳";
+        hero.skill3 = "加攻击并吸血";
+
         //通过map转成JSON结构字符串
         //1.创建gson对象
         Gson gson=new GsonBuilder().create();
         //2.使用toJson方法把键值对转成字符串
-        String str=gson.toJson(hashMap);
-        System.out.println(str);
+        String str1=gson.toJson(hashMap);
+        String str2=gson.toJson(hero);
+        System.out.println(str2);
         //{"skill3":"加攻击并吸血","skill4":"加攻速","skill1":"剑气","skill2":"三段跳","name":"曹操"}
-        //键值对之间得顺序不保证
     }
 }
