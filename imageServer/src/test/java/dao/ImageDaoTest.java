1840f25d8f6bf16b7133d7660a3b57a9c0404184
yologithup <18821769219@163.com>
2020-06-02 20:19:59 +0800

    -

---
+++ imageServer/src/test/java/dao/ImageDaoTest.java
@@ -0,0 +1,30 @@
+package dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author yolo
+ * @date 2020/6/2-19:36
+ */
+class ImageDaoTest {
+
+    @org.junit.jupiter.api.Test
+    void insert() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void selectAll() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void selectOne() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void delete() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void selectByMd5() {
+    }
+}
